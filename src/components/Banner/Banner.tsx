ef5f96bcadfc536fcc9ebaeeada1cf4ee8c22ced
Adityanarayan Tiwari <adityant17121997@gmail.com>
2023-08-17 21:55:43 +0530

    placeholder

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,8 +1,9 @@
 import "./banner.scss";
 import { Col, Container, Row } from "react-bootstrap";
 import { Fade, Slide } from "react-awesome-reveal";
-
-const Profile = `https://github.com/iamadityanarayan/Images/blob/main/Murali%20Krishna/Home%20Page/profile.jpg?raw=true`
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import UserPlaceholder from "../../assets/images/placeholders/home/pprofile.png";
+const Profile = `https://github.com/iamadityanarayan/Images/blob/main/Murali%20Krishna/Home%20Page/profile.jpg?raw=true`;
 
 const Banner = () => {
   return (
@@ -12,15 +13,23 @@ const Banner = () => {
         <Row className="d-none d-lg-flex center justify-content-center vh-100 align-items-center mx-auto">
           <Col lg={5} className="mx-auto center-l">
             <Slide direction="right" className="__img text-end rounded-circle">
-              <img src={Profile} className="img-holder rounded-circle" alt="" />
+              {/* <img src={Profile} className="img-holder rounded-circle" alt="" /> */}
+              <LazyLoadImage
+                src={Profile}
+                // width={600}
+                // height={400}
+                className="img-holder rounded-circle"
+                placeholderSrc={UserPlaceholder}
+                alt="Image Alt"
+              />
             </Slide>
           </Col>
           <Col lg={7}>
             <div className="text-content">
               <Fade cascade damping={0.6} delay={1500}>
-              <div className="mb-4">
-                <span>Hello, I am Murali Krishna!</span>
-              </div>
+                <div className="mb-4">
+                  <span>Hello, I am Murali Krishna!</span>
+                </div>
                 <p>I design</p>
                 <p>functional and visually</p>
                 <p>pleasing interfaces for mobile and web.</p>
@@ -32,10 +41,18 @@ const Banner = () => {
         <Row className="justify-content-center d-lg-none flex-column  pt-5 align-items-center mx-auto">
           <Col sm={12} lg={5} className="mx-auto center-left">
             <div className="text-center rounded-circle animate__animated animate__slideInRight">
-              <img
+              {/* <img
                 src={Profile}
                 className="img-holder-2 rounded-circle"
                 alt=""
+              /> */}
+              <LazyLoadImage
+                src={Profile}
+                // width={600}
+                // height={400}
+                className="img-holder rounded-circle"
+                placeholderSrc={UserPlaceholder}
+                alt="Image Alt"
               />
             </div>
           </Col>
